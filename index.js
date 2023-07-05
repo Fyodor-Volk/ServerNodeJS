2e8a928d76adce0c2a14fd6aa6391a4add1f9e97
Fyodor_Volk <keytashi17@gmail.com>
2023-07-05 21:39:32 +0700

    Create read url

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 let server = http.createServer((req, res) =>{
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-    res.end('<iframe frameborder="0" style="border:none;width:100%;height:180px;" width="100%" height="180" src="https://music.yandex.ru/iframe/#track/90464681/17882989">Слушайте <a href="https://music.yandex.ru/album/17882989/track/90464681">Endless Love</a> — <a href="https://music.yandex.ru/artist/8887635">DVRST</a> на Яндекс Музыке</iframe>')
-
+    
+    if(req.url == '/')
+        fs.createReadStream('./templates/index.html').pipe(res) 
+    else if(req.url == '/about')
+        fs.createReadStream('./templates/about.html').pipe(res) 
 })
 const PORT = 4000;
 const HOST = 'localhost';
